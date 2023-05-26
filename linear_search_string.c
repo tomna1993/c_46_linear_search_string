5a721cfafdd3186b8e6e23d1f4d41b0b50e803c4
TomasNagy <tomi.nagy@outlook.com>
2023-05-26 18:52:03 +0200

    Implement linear search algorithm

--- linear_search_string.c
+++ linear_search_string.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 #define ARRAY_LENGTH 6
 
@@ -9,4 +10,21 @@ int main(void)
 
     string get_user_string = get_string("String: ");
 
+    for (int i = 0; i < ARRAY_LENGTH; i++)
+    {
+        // if (names[i] == get_user_string) would not work to compare strings
+        // Have to use strcmp(str1, str2) from string.h
+        // if Return value < 0 then it indicates str1 is less than str2.
+        // if Return value > 0 then it indicates str2 is less than str1.
+        // if Return value = 0 then it indicates str1 is equal to str2.
+       
+        if (strcmp(names[i], get_user_string) == 0)
+        {
+            printf ("Found!\n");
+            return 0;
+        }
+    }
+
+    printf ("Not found!\n");
+    return 1;
 }
